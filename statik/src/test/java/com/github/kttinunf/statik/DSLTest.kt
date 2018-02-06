5c4f83880eff7f9b0f60e7c5037bb74bf5325689
Kittinun Vantasin <kittinun.f@gmail.com>
2018-02-06 16:27:14 +0900

    Make tests great again

--- statik/src/test/java/com/github/kttinunf/statik/DSLTest.kt
+++ statik/src/test/java/com/github/kttinunf/statik/DSLTest.kt
@@ -1,5 +1,7 @@
 package com.github.kttinunf.statik
 
+import android.graphics.Typeface
+import android.view.Gravity
 import com.github.kittinunf.statik.dsl.row
 import com.github.kittinunf.statik.model.Row
 import org.hamcrest.CoreMatchers.equalTo
@@ -38,20 +40,31 @@ class DSLTest {
     @Test
     fun rowCommand_shouldBuildTextTypeWithCorrectAttribute() {
         val r = row {
-            primaryText = "foo"
             primaryTextAttribute {
                 color = "#112233"
+                sizeSP = 10.0f
+                typeface = Typeface.DEFAULT_BOLD
+                gravity = Gravity.START
             }
 
             secondaryTextAttribute {
                 color = "#223344"
+                sizeSP = 20.0f
+                typeface = Typeface.SERIF
+                gravity = Gravity.END
             }
         }
 
         val t = r.type as Row.Type.Text
 
         assertThat(t.primaryTextAttribute?.textColor, equalTo("#112233"))
-        assertThat(t.secondaryTextAttribute?.textColor, equalTo("#112233"))
+        assertThat(t.secondaryTextAttribute?.textColor, equalTo("#223344"))
+        assertThat(t.primaryTextAttribute?.textSizeSP, equalTo(10.0f))
+        assertThat(t.secondaryTextAttribute?.textSizeSP, equalTo(20.0f))
+        assertThat(t.primaryTextAttribute?.typeface, equalTo(Typeface.DEFAULT_BOLD))
+        assertThat(t.secondaryTextAttribute?.typeface, equalTo(Typeface.SERIF))
+        assertThat(t.primaryTextAttribute?.textGravity, equalTo(Gravity.START))
+        assertThat(t.secondaryTextAttribute?.textGravity, equalTo(Gravity.END))
     }
 
 }
