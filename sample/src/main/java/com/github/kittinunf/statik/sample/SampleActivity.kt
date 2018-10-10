655a344d2571b1fce07483dcb0641136d94c69fd
Anthony Beylerian <anthony.beylerian@gmail.com>
2018-10-10 23:14:37 +0900

    :memo: Rename [statik] to [adapter] (#71)

--- sample/src/main/java/com/github/kittinunf/statik/sample/SampleActivity.kt
+++ sample/src/main/java/com/github/kittinunf/statik/sample/SampleActivity.kt
@@ -2,8 +2,7 @@ package com.github.kittinunf.statik.sample
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.LinearLayoutManager
-import com.github.kittinunf.statik.dsl.statik
+import com.github.kittinunf.statik.dsl.adapter
 import kotlinx.android.synthetic.main.activity_list.list
 
 class SampleActivity : AppCompatActivity() {
@@ -13,11 +12,6 @@ class SampleActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_list)
 
-        val adapter = statik {}
-
-        list.also {
-            it.layoutManager = LinearLayoutManager(this)
-            it.adapter = adapter
-        }
+        list.adapter = adapter {}
     }
 }
